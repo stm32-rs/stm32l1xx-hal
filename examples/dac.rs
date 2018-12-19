437f6105926fa1b2309c2ade5a1b248d8a8ba8cb
Vitaly Domnikov <dotcypress@gmail.com>
2018-12-19 01:05:12 -0800

    update dac example

--- examples/dac.rs
+++ examples/dac.rs
@@ -1,4 +1,4 @@
-// #![deny(warnings)]
+#![deny(warnings)]
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
@@ -10,22 +10,13 @@ extern crate stm32l1xx_hal as hal;
 
 use hal::prelude::*;
 use hal::stm32;
-use hal::rcc::SysClkSource;
 use rt::entry;
 
 enum Dir { Up, Down }
 
 #[entry]
 fn main() -> ! {
-    let mut val: u16 = 0;
-    let mut dir = Dir::Up;
-
     let dp = stm32::Peripherals::take().unwrap();
-    
-    let rcc = dp.RCC.constrain();
-    rcc.cfgr
-        .sys_clk_src(SysClkSource::HSI)
-        .freeze();
 
     let gpioa = dp.GPIOA.split();
     let (mut dac1, mut dac2) = dp.DAC.dac((gpioa.pa4, gpioa.pa5));
@@ -33,17 +24,20 @@ fn main() -> ! {
     dac1.enable();
     dac2.enable();
     
+    let mut val: u16 = 0;
+    let mut dir = Dir::Up;
+    
     loop {
-        match val {
-            _ => (),
-            0 => dir = Dir::Up,
-            core::u16::MAX => dir = Dir::Down,
-        };
-        match dir {
-            Dir::Up => val += 1,
-            Dir::Down => val -= 1,
-        }
         dac1.set(val);
         dac2.set(core::u16::MAX - val);
+        match val {
+            0 => dir = Dir::Up,
+            core::u16::MAX => dir = Dir::Down,
+            _ => (),
+        };
+        match dir {
+            Dir::Up => val += 85,
+            Dir::Down => val -= 85,
+        }
     }
 }
