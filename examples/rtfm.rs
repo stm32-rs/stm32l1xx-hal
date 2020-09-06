190ea8ec163a61a9dc8eae63d04367e30452062c
Sergey Matyukevich <geomatsi@gmail.com>
2020-09-06 20:32:34 +0300

    examples: update rtfm example
    Update RTFM to v5.5 and switch to its new name RTIC.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- examples/rtfm.rs
+++ examples/rtfm.rs
@@ -6,11 +6,11 @@ extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate cortex_m_semihosting;
 extern crate panic_semihosting;
-extern crate rtfm;
+extern crate rtic;
 extern crate stm32l1xx_hal as hal;
 
 use cortex_m_semihosting::hprintln;
-use rtfm::app;
+use rtic::app;
 
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::digital::v2::ToggleableOutputPin;
@@ -22,50 +22,63 @@ use hal::rcc::Config;
 use hal::stm32;
 use hal::timer::Timer;
 
-#[app(device = hal::stm32)]
+#[app(device = hal::stm32, peripherals = true)]
 const APP: () = {
-    static mut DELTA: u32 = 0;
-    static mut TIMER: Timer<stm32::TIM2> = ();
-    static mut TICKS_LED: PB6<Output<PushPull>> = ();
-    static mut BUSY_LED: PB7<Output<PushPull>> = ();
-    static EXTI: stm32::EXTI = ();
+    struct Resources {
+        // resources
+        #[init(0)]
+        DELTA: u32,
+
+        // late resources
+        TIMER: Timer<stm32::TIM2>,
+        TICKS_LED: PB6<Output<PushPull>>,
+        BUSY_LED: PB7<Output<PushPull>>,
+        EXTI: stm32::EXTI,
+    }
 
     #[init]
-    fn init() {
-        let mut rcc = device.RCC.freeze(Config::hsi());
+    fn init(cx: init::Context) -> init::LateResources {
+        let mut rcc = cx.device.RCC.freeze(Config::hsi());
 
-        let gpiob = device.GPIOB.split();
-        let mut timer = device.TIM2.timer(1.hz(), &mut rcc);
+        let gpiob = cx.device.GPIOB.split();
+        let mut timer = cx.device.TIM2.timer(1.hz(), &mut rcc);
 
         timer.listen();
-        device.EXTI.listen(0, TriggerEdge::Rising);
+        cx.device.EXTI.listen(0, TriggerEdge::Rising);
 
-        TICKS_LED = gpiob.pb6.into_push_pull_output();
-        BUSY_LED = gpiob.pb7.into_push_pull_output();
-        TIMER = timer;
-        EXTI = device.EXTI;
+        let TICKS_LED = gpiob.pb6.into_push_pull_output();
+        let BUSY_LED = gpiob.pb7.into_push_pull_output();
+        let TIMER = timer;
+        let EXTI = cx.device.EXTI;
+
+        init::LateResources {
+            TIMER,
+            TICKS_LED,
+            BUSY_LED,
+            EXTI,
+        }
     }
 
-    #[interrupt(resources = [TIMER, TICKS_LED, DELTA])]
-    fn TIM2() {
-        *resources.DELTA += 1;
+    #[task(binds = TIM2, resources = [TIMER, TICKS_LED, DELTA])]
+    fn tim2_handler(cx: tim2_handler::Context) {
+        *cx.resources.DELTA += 1;
 
-        resources.TICKS_LED.toggle().unwrap();
-        resources.TIMER.clear_irq();
+        cx.resources.TICKS_LED.toggle().unwrap();
+        cx.resources.TIMER.clear_irq();
     }
 
-    #[interrupt(resources = [EXTI, BUSY_LED, DELTA])]
-    fn EXTI0() {
-        resources.BUSY_LED.set_high().unwrap();
-        hprintln!("Δ: {}", resources.DELTA).unwrap();
-        resources.BUSY_LED.set_low().unwrap();
+    #[task(binds = EXTI0, resources = [EXTI, BUSY_LED, DELTA])]
+    fn exti0_handler(cx: exti0_handler::Context) {
+        cx.resources.BUSY_LED.set_high().unwrap();
+        hprintln!("Δ: {}", cx.resources.DELTA).unwrap();
+        cx.resources.BUSY_LED.set_low().unwrap();
 
-        *resources.DELTA = 0;
-        resources.EXTI.clear_irq(0);
+        *cx.resources.DELTA = 0;
+        cx.resources.EXTI.clear_irq(0);
     }
 
     #[idle]
-    fn idle() -> ! {
+    fn idle(_: idle::Context) -> ! {
         loop {}
     }
 };
